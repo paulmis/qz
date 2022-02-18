ed3d7e5e1405ce00a45c706c39d67dc4a067ebd0
rstular <r.stular@student.tudelft.nl>
2022-02-18 19:46:14 +0100

    feat(entity): Add GameStatus enum

---
+++ commons/src/main/java/commons/game/GameStatus.java
@@ -0,0 +1,19 @@
+package commons.game;
+
+/**
+ * Enum stores the status of a Game object.
+ */
+public enum GameStatus {
+    /**
+     * Game has been created, but not started.
+     */
+    CREATED,
+    /**
+     * Game has been started and is in-progress.
+     */
+    ONGOING,
+    /**
+     * Game has been completed.
+     */
+    FINISHED
+}
