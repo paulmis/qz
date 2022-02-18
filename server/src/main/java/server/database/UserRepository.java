dd5ac2e4d9582138c3d9fab9f684d21bbc9cecdc
Cojocaru Alexandru-Gabriel <A.G.Cojocaru-2@student.tudelft.nl>
2022-02-18 17:58:06 +0100

    Added newline and changed id

--- server/src/main/java/server/database/UserRepository.java
+++ server/src/main/java/server/database/UserRepository.java
@@ -1,2 +1,9 @@
-package server.database;public interface UserRepository {
-}
+package server.database;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import commons.User;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {}
