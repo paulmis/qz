9842ed5875dda9bcb52cb181141c6b3391b3e426
Cojocaru Alexandru-Gabriel <A.G.Cojocaru-2@student.tudelft.nl>
2022-02-19 22:18:34 +0100

    Added more javadoc and fixed style issues

--- commons/src/main/java/commons/User.java
+++ commons/src/main/java/commons/User.java
@@ -2,6 +2,7 @@ package commons;
 
 import java.util.UUID;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -12,20 +13,42 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.ToString;
 
+
 /**
  * User entity - describes an user in the context of the entire application.
  */
 
 @NoArgsConstructor
-@AllArgsConstructor(access = AccessLevel.PUBLIC)
-@ToString(includeFieldNames = false)
-@Data public class User {
+@AllArgsConstructor (access = AccessLevel.PUBLIC)
+@ToString (includeFieldNames = false)
+@Data
+@Entity
+public class User {
+    /**
+     * id - random unique uuid assigned to a certain player.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+
+    /**
+     * email - string used for authentication purposes representing the email of the user.
+     */
     @Column(nullable = false, unique = true)
     private String email;
+
+    /**
+     * password - string representing user's salted password.
+     */
     @NonNull private String password;
+
+    /**
+     * score - integer representing a player's total score.
+     */
     private int score;
+
+    /**
+     * id - random unique uuid assigned to a certain player.
+     */
     private int gamesPlayed;
 }
