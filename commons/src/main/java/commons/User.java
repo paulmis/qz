455aedebb02fc3b5c9392d0ab671d692ada90550
Cojocaru Alexandru-Gabriel <A.G.Cojocaru-2@student.tudelft.nl>
2022-02-19 12:48:59 +0100

    Deleted not used imports

--- commons/src/main/java/commons/User.java
+++ commons/src/main/java/commons/User.java
@@ -2,21 +2,15 @@ package commons;
 
 import java.util.UUID;
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.ToString;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-import org.apache.commons.lang3.builder.ToStringBuilder;
 
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
