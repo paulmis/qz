b6af849c3b0db3575da91d2fd1930a2e555ae4da
Cojocaru Alexandru-Gabriel <A.G.Cojocaru-2@student.tudelft.nl>
2022-02-19 14:33:02 +0100

    Added javadoc

--- server/src/main/java/server/database/UserRepository.java
+++ server/src/main/java/server/database/UserRepository.java
@@ -1,9 +1,13 @@
 package server.database;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import commons.User;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+/**
+ * User Repository - Interface that initializes repository.
+ */
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {}
