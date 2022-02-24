a3e7489410346a848d0e6dfa8e8182deb68d9de0
Giacomo Pezzali <g.pezzali@student.tudelft.nl>
2022-02-24 12:08:11 +0100

    Repository ids set to UUID
    Repository ids set to UUID to match entities id type

--- server/src/main/java/server/database/repositories/game/GamePlayerRepository.java
+++ server/src/main/java/server/database/repositories/game/GamePlayerRepository.java
@@ -1,10 +1,11 @@
 package server.database.repositories.game;
 
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import server.database.entities.game.GamePlayer;
 
 /**
  * JPA interface for accessing GamePlayer entities.
  */
-public interface GamePlayerRepository extends JpaRepository<GamePlayer, Long> {
+public interface GamePlayerRepository extends JpaRepository<GamePlayer, UUID> {
 }
