36c3ab9fd23f50f2c39c96e1f82d55aa9b62375d
Giacomo Pezzali <g.pezzali@student.tudelft.nl>
2022-02-25 17:59:14 +0100

    Various fixes
    Endpoint output changed to ResponseEntity to handle server errors. Autowired annotation used to connect the repository.

--- server/src/main/java/server/database/repositories/game/GameRepository.java
+++ server/src/main/java/server/database/repositories/game/GameRepository.java
@@ -1,9 +1,13 @@
 package server.database.repositories.game;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import server.database.entities.game.Game;
+import server.database.entities.game.GameStatus;
 
 /**
  * JPA repository for interacting with Game entities in the database.
  */
-public interface GameRepository extends JpaRepository<Game, Long> {}
+public interface GameRepository extends JpaRepository<Game, Long> {
+    List<Game> findAllByStatus(GameStatus status);
+}
