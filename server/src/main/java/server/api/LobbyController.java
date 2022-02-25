b96f81ed093189cb3a4b2d3d1dab56101381668e
Giacomo Pezzali <g.pezzali@student.tudelft.nl>
2022-02-25 17:59:14 +0100

    Various fixes
    Endpoint output changed to ResponseEntity to handle server errors. Autowired annotation used to connect the repository.

--- server/src/main/java/server/api/LobbyController.java
+++ server/src/main/java/server/api/LobbyController.java
@@ -1,7 +1,13 @@
 package server.api;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import server.database.entities.game.Game;
 import server.database.entities.game.GameStatus;
@@ -15,32 +21,40 @@ made following https://spring.io/guides/tutorials/rest/
  * LobbyController, expose endpoints for lobbies.
  */
 @RestController
+@RequestMapping("/api/lobbies")
 public class LobbyController {
 
     /**
-     * repository of the lobbies.
+     * Repository of the lobbies.
      */
-    private final GameRepository lobbyRepo;
+    @Autowired
+    private GameRepository lobbyRepository;
 
     /**
-     * Constructor of the LobbyController.
-     *
-     * @param lobbyRepo the game repository.
-     */
-    LobbyController(GameRepository lobbyRepo) {
-        this.lobbyRepo = lobbyRepo;
-    }
-
-    /**
-     * endpoint for available lobbies.
+     * Endpoint for testing. To be removed.
      *
      * @return a list of available lobbies.
      */
-    @GetMapping("/lobby")
-    List<Game> availableLobbies() {
+    @GetMapping(path = {"", "/test/{param}"})
+    ResponseEntity<List<Game>> testEndpoint(
+            @RequestParam(required = false) Integer id, @PathVariable(required = false) String param) {
+        System.out.println(param + ", " + id);
+        List<Game> lobbies = lobbyRepository.findAllByStatus(GameStatus.CREATED);
+        if (id == 42) {
+            return new ResponseEntity<>(null, HttpStatus.I_AM_A_TEAPOT);
+        }
+        return new ResponseEntity<>(lobbies, HttpStatus.OK);
+    }
+
+    /**
+     * Endpoint for available lobbies.
+     *
+     * @return a list of available lobbies.
+     */
+    @GetMapping(path = {"", "/available"})
+    ResponseEntity<List<Game>> availableLobbies() {
         // It should return games with status Created
-        List<Game> lobbies = lobbyRepo.findAll();
-        lobbies.removeIf(game -> game.getStatus() == GameStatus.CREATED);
-        return lobbies;
+        List<Game> lobbies = lobbyRepository.findAllByStatus(GameStatus.CREATED);
+        return new ResponseEntity<>(lobbies, HttpStatus.OK);
     }
 }
--- server/src/main/java/server/database/repositories/game/GameRepository.java
+++ server/src/main/java/server/database/repositories/game/GameRepository.java
@@ -1,10 +1,14 @@
 package server.database.repositories.game;
 
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import server.database.entities.game.Game;
+import server.database.entities.game.GameStatus;
 
 /**
  * JPA repository for interacting with Game entities in the database.
  */
 public interface GameRepository extends JpaRepository<Game, UUID> {}
+    List<Game> findAllByStatus(GameStatus status);
+}
