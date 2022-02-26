959eb9178a704c2bf45bdac1aeaca1d59be342d9
rstular <r.stular@student.tudelft.nl>
2022-02-26 16:12:52 +0100

    fix(dto): Fix annotations on game configuration DTOs

--- commons/src/main/java/commons/entities/game/configuration/SurvivalGameConfigurationDTO.java
+++ commons/src/main/java/commons/entities/game/configuration/SurvivalGameConfigurationDTO.java
@@ -2,11 +2,13 @@ package commons.entities.game.configuration;
 
 import commons.entities.utils.DTO;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 /**
  * Data transfer object for the survival-mode game configuration.
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 public class SurvivalGameConfigurationDTO extends GameConfigurationDTO implements DTO {
