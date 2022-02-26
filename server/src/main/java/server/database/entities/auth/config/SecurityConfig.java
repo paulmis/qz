5f965dde5cf2a59f942d1363bb996a592567dbd2
Paul Misterka <p.m.misterka@student.tudelft.nl>
2022-02-26 18:33:45 +0100

    implement UserDetailsService to provide auth DAOs

--- server/src/main/java/server/database/entities/auth/config/SecurityConfig.java
+++ server/src/main/java/server/database/entities/auth/config/SecurityConfig.java
@@ -19,18 +19,22 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired private AuthFilter authFilter;
+    @Autowired private CustomUserDetailsService customUserDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .httpBasic().disable()
+                .cors()
+                .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeHttpRequests()
                 .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/user/**").hasRole("USER")
                 .and()
+                .userDetailsService(customUserDetailsService)
                 .exceptionHandling()
                 .authenticationEntryPoint(
                         // Rejecting request as unauthorized when entry point is reached
--- server/src/main/java/server/database/repositories/UserRepository.java
+++ server/src/main/java/server/database/repositories/UserRepository.java
@@ -14,6 +14,14 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
 
     /**
+     * Checks if a user with the provided email exists.
+     *
+     * @param email the user's email
+     * @return whether the user exists
+     */
+    boolean existsByEmail(String email);
+
+    /**
      * Checks if the provided email and username are unique.
      *
      * @param email the email to check
