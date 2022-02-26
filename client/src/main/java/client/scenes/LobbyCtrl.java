dffd29eb64befa0a6017fe9dc84e4889ff412815
Paul Misterka <p.m.misterka@student.tudelft.nl>
2022-02-26 14:30:25 +0100

    switch to jfxbuttons, faicons

--- client/src/main/java/client/scenes/LobbyCtrl.java
+++ client/src/main/java/client/scenes/LobbyCtrl.java
@@ -1,7 +1,15 @@
 package client.scenes;
 
+import com.jfoenix.controls.JFXButton;
+
 /**
  * Lobby controller.
  */
 public class LobbyCtrl {
+    public JFXButton settingsButton;
+    public JFXButton userButton;
+    public JFXButton copyLinkButton;
+    public JFXButton startButton;
+    public JFXButton lobbySettingsButton;
+    public JFXButton disbandButton;
 }
