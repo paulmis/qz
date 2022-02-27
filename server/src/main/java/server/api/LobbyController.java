47b33f62264f1992694960de1ea16209659748ff
Giacomo Pezzali <g.pezzali@student.tudelft.nl>
2022-02-27 10:10:48 +0100

    Clearer join process
    Improved and fixed joining process, it now uses the userId to associate a user with a player and updated the repositories.

--- server/src/main/java/server/api/LobbyController.java
+++ server/src/main/java/server/api/LobbyController.java
@@ -12,12 +12,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import server.database.entities.User;
 import server.database.entities.game.Game;
 import server.database.entities.game.GamePlayer;
+import server.database.repositories.UserRepository;
+import server.database.repositories.game.GamePlayerRepository;
 import server.database.repositories.game.GameRepository;
 
 /*
@@ -26,16 +29,26 @@ made following https://spring.io/guides/tutorials/rest/
 
 /**
  * LobbyController, expose endpoints for lobbies.
+ * Lobbies are games that haven't started yet.
  */
 @RestController
 @RequestMapping("/api/lobbies")
 public class LobbyController {
 
     /**
-     * Repository of the lobbies.
+     * Repository of the games.
      */
-    @Autowired
-    private GameRepository lobbyRepository;
+    @Autowired private GameRepository gameRepository;
+
+    /**
+     * Repository of the users.
+     */
+    @Autowired private UserRepository userRepository;
+
+    /**
+     * Repository of the players.
+     */
+    @Autowired private GamePlayerRepository gamePlayerRepository;
 
     /**
      * Endpoint for available lobbies.
@@ -46,7 +59,7 @@ public class LobbyController {
     ResponseEntity<List<GameDTO>> availableLobbies() {
         // ToDo: it should respond only to authenticated users
         // It returns games with status Created
-        List<GameDTO> lobbies = lobbyRepository.findAllByStatus(GameStatus.CREATED)
+        List<GameDTO> lobbies = gameRepository.findAllByStatus(GameStatus.CREATED)
                 .stream().map(g -> g.getDTO()).collect(Collectors.toList());
         return new ResponseEntity<>(lobbies, HttpStatus.OK);
     }
@@ -60,7 +73,7 @@ public class LobbyController {
     @GetMapping(path = {"", "/{lobbyId}"})
     ResponseEntity<GameDTO> lobbyInfo(@PathVariable @NonNull UUID lobbyId) {
         // ToDo: it should respond only to authenticated users
-        Game lobby = lobbyRepository.getById(lobbyId);
+        Game lobby = gameRepository.getById(lobbyId);
         if (lobby == null) {
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
@@ -70,22 +83,49 @@ public class LobbyController {
     /**
      * Endpoint to allow a user to join a game.
      *
-     * @param playerData information on the player.
+     * @param playerData information on the joining player (e.g. their nickname).
      * @param lobbyId    UUID of the lobby to join.
+     * @param userId     UUID of the user that has to join.
      * @return true if the join was successful, false otherwise.
      */
-    @PostMapping(path = {"", "/join/{lobbyId}"})
+    @PutMapping(path = {"", "/{lobbyId}/join/{userId}"})
     ResponseEntity<Boolean> joinLobby(
-            @RequestBody @NonNull GamePlayerDTO playerData, @PathVariable @NonNull UUID lobbyId) {
+            @RequestBody @NonNull GamePlayerDTO playerData,
+            @PathVariable @NonNull UUID lobbyId,
+            @PathVariable @NonNull UUID userId) {
         // ToDo: it should respond only to authenticated users
-        // ToDo: should maybe check whether the user is allowed to join? Or it's the client's job?
-        Game toJoin = lobbyRepository.getById(lobbyId);
+        // ToDo: it should maybe check whether the user is allowed to join? Or is it the client's job?
+
+        // Find lobby to join
+        Game toJoin = gameRepository.getById(lobbyId);
         if (toJoin == null) {
-            return new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
+            // No lobby.
+            return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
         }
+        if (toJoin.getStatus() != GameStatus.CREATED) {
+            // Game already started.
+            return new ResponseEntity<>(false, HttpStatus.CONFLICT);
+        }
+
+        // Find user trying to join
+        User joiningUser = userRepository.getById(userId);
+        if (joiningUser == null) {
+            // No user.
+            return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
+        }
+
+        // Create player
         GamePlayer player = new GamePlayer(playerData);
+        player.setUser(joiningUser);
+
+        // Try to join the game
         boolean success = toJoin.add(player);
-        lobbyRepository.save(toJoin);
-        return new ResponseEntity<>(success, success ? HttpStatus.OK : HttpStatus.BAD_REQUEST);
+        if (success) {
+            // Update repositories
+            gameRepository.save(toJoin);
+            gamePlayerRepository.save(player);
+            userRepository.save(joiningUser);
+        }
+        return new ResponseEntity<>(success, success ? HttpStatus.OK : HttpStatus.CONFLICT);
     }
 }
