fb26868951c360fd59ff8f284422d20b739ec425
Aakanksh Singh <a.singh-27@student.tudelft.nl>
2022-02-27 05:39:48 +0100

    Added question controller
    Added code for question controller and get mapping

--- server/src/main/java/server/api/QuestionController.java
+++ server/src/main/java/server/api/QuestionController.java
@@ -1,4 +1,43 @@
 package server.api;
 
+import commons.entities.QuestionDTO;
+import lombok.Generated;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import server.database.entities.question.Question;
+import server.database.repositories.question.QuestionRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * QuestionController, controller for all api endpoints of question creation
+ */
+@RestController
+@RequestMapping("api/questions")
+@Generated
 public class QuestionController {
+
+    /**
+     * Question repository
+     */
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    /**
+     * Endpoint for current question
+     *
+     * @return the current question object
+     */
+    @GetMapping(path = {"","/questions/{num}"})
+    ResponseEntity<QuestionDTO> currentQuestion(@PathVariable UUID id) {
+        QuestionDTO currentQuestion = questionRepository.findById(id).get().getDTO();
+        return new ResponseEntity<>(currentQuestion, HttpStatus.OK);
+    }
+
 }
