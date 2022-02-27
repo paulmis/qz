4d10b346a3e77624690a16e5f35e6215ff897a8d
Aakanksh Singh <a.singh-27@student.tudelft.nl>
2022-02-27 05:39:48 +0100

    Added question controller
    Added code for question controller and get mapping

--- server/src/main/java/server/api/AnswerController.java
+++ server/src/main/java/server/api/AnswerController.java
@@ -1,4 +1,15 @@
 package server.api;
 
+import commons.entities.AnswerDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import server.database.repositories.question.QuestionRepository;
+
+/**
+ * AnswerController, controller for all api endpoints of question answers
+ */
+@RestController
+@RequestMapping("/api/answers")
 public class AnswerController {
 }
