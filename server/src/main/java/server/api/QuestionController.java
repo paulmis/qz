c63325a45df949b4640e8e30252c7ec67580b6f9
Aakanksh Singh <a.singh-27@student.tudelft.nl>
2022-02-27 05:39:48 +0100

    Added question controller
    Added code for question controller and get mapping

--- server/src/main/java/server/api/QuestionController.java
+++ server/src/main/java/server/api/QuestionController.java
@@ -5,7 +5,10 @@ import lombok.Generated;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import server.database.entities.question.Question;
 import server.database.repositories.question.QuestionRepository;
 
@@ -36,4 +39,5 @@ public class QuestionController {
         QuestionDTO currentQuestion = questionRepository.findById(id).get().getDTO();
         return new ResponseEntity<>(currentQuestion, HttpStatus.OK);
     }
+
 }
