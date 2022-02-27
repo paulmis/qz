b541c0b166f6f688e2330c15c61988ded196a78a
Aakanksh Singh <a.singh-27@student.tudelft.nl>
2022-02-27 16:57:47 +0100

    Minor changes
    small changes

--- server/src/main/java/server/api/AnswerController.java
+++ server/src/main/java/server/api/AnswerController.java
@@ -2,6 +2,9 @@ package server.api;
 
 import commons.entities.AnswerDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import server.database.repositories.question.QuestionRepository;
@@ -10,6 +13,18 @@ import server.database.repositories.question.QuestionRepository;
  * AnswerController, controller for all api endpoints of question answers
  */
 @RestController
-@RequestMapping("/api/answers")
+//@RequestMapping("/api/answers")
 public class AnswerController {
+
+    /**
+     * Question repository
+     */
+    @Autowired
+    private QuestionRepository questionRepository;
+
+//    @PostMapping("userAnswer")
+//    public ResponseEntity<String> answerQuestion(@RequestBody AnswerDTO answerData) {
+//
+//    }
+
 }
