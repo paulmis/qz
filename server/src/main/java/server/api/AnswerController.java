9dd3beb4eaf87ac9956b16e289e801a3769ba2d8
Aakanksh Singh <a.singh-27@student.tudelft.nl>
2022-03-03 22:56:57 +0100

    Fixed some problems
    Fixed checkstyle pipeline errors

--- server/src/main/java/server/api/AnswerController.java
+++ server/src/main/java/server/api/AnswerController.java
@@ -1,46 +1,51 @@
 package server.api;
 
-import commons.entities.ActivityDTO;
 import commons.entities.AnswerDTO;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import server.database.entities.game.Game;
-import server.database.repositories.game.GameRepository;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import server.database.entities.game.Game;
+import server.database.repositories.game.GameRepository;
 
 /**
- * AnswerController, controller for all api endpoints of question answers
+ * AnswerController, controller for all api endpoints of question answers.
  */
 @RestController
 @RequestMapping("/api")
 public class AnswerController {
 
     /**
-     * Game repository
+     * Game repository import.
      */
     @Autowired
     private GameRepository gameRepository;
 
+    /**
+     * Sends the users answers to the server.
+     * @param answerData Contains the players answer in AnswerDTO format
+     * @param gameId This is the gameId of the game being played
+     * @return ok status if successful, not found status if game doesn't exist
+     */
     @PostMapping("game/{gameId}/answer")
     public ResponseEntity<List<AnswerDTO>> answerQuestion(@RequestBody AnswerDTO answerData,
                                                           @PathVariable @NonNull UUID gameId) {
-        /**
-         * Check if game exists
-         */
+        //Check if game exists.
         Optional<Game> game = gameRepository.findById(gameId);
-        if(!game.isPresent()) {
+        if (!game.isPresent()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
         // ToDo: send answer data to server
-        /**
-         * Send 200 status if answer is sent successfully
-         */
+        //Send 200 status if answer is sent successfully.
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
--- server/src/main/java/server/api/QuestionController.java
+++ server/src/main/java/server/api/QuestionController.java
@@ -5,28 +5,30 @@ import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.Optional;
+import java.util.UUID;
 import server.database.entities.question.Question;
 import server.database.repositories.question.QuestionRepository;
 
-import java.util.Optional;
-import java.util.UUID;
-
 /**
- * QuestionController, controller for all api endpoints of question
+ * QuestionController, controller for all api endpoints of question.
  */
 @RestController
 @RequestMapping("api/questions")
 public class QuestionController {
 
     /**
-     * Question repository
+     * Question repository import.
      */
     @Autowired
     private QuestionRepository questionRepository;
 
     /**
-     * Endpoint for retrieving the current question
+     * Endpoint for retrieving the current question.
      *
      * @param questionId the UUID of the current question
      * @return information/object of the current question
@@ -34,9 +36,11 @@ public class QuestionController {
     @GetMapping("/{questionId}")
     ResponseEntity<QuestionDTO> currentQuestion(@PathVariable @NonNull UUID questionId) {
         Optional<Question> question = questionRepository.findById(questionId);
-        if(!question.isPresent()) {
+        //Check if question exists.
+        if (!question.isPresent()) {
             return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
+        //Send 200 status if question exists and sends the question payload back.
         return ResponseEntity.ok(question.get().getDTO());
     }
 }
