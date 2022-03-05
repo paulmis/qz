eac088c15c30a19aaf75f4118ac9424f6e311273
Aakanksh Singh <a.singh-27@student.tudelft.nl>
2022-03-05 21:21:00 +0100

    Fixed some checkstyle errors
    answerController and questionControllerTest checkstyle error fix

--- server/src/test/java/server/api/QuestionControllerTest.java
+++ server/src/test/java/server/api/QuestionControllerTest.java
@@ -14,8 +14,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-
-import commons.entities.game.GameStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
