7169dbc83406caa629c1474b5de012037d8cfbdf
Giacomo Pezzali <g.pezzali@student.tudelft.nl>
2022-03-08 00:18:50 +0100

    Adapted code to new GameService
    Changed code to adapt to new code.

--- server/src/main/java/server/services/AnswerService.java
+++ server/src/main/java/server/services/AnswerService.java
@@ -1,13 +1,12 @@
 package server.services;
 
 import commons.entities.AnswerDTO;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import server.database.entities.game.Game;
+import server.database.entities.question.Question;
 import server.database.repositories.game.GameRepository;
 
 /**
@@ -25,14 +24,17 @@ public class AnswerService {
      * @param gameId id of the game that needs the answer.
      */
     public void sendAnswer(UUID gameId) {
-        // ToDo: get SSEManager to have access to sending methods
         Optional<Game> myGame = gameRepository.findById(gameId);
         if (myGame.isPresent()) {
-            List<UUID> players = myGame.get().getPlayers().stream().map(p -> p.getUser().getId())
-                    .collect(Collectors.toList());
-            // ToDo: get GameService to retrieve current question
-            //AnswerDTO answer = myGame.get().getQuestions().get(myGame.get().CurrentQuestion()).getRightAnswer();
-            // SSEManager::send(players, answer);
+            // Retrieve current question
+            Optional<Question> currentQuestion = myGame.get().getQuestion();
+            if (!currentQuestion.isPresent()) {
+                // Game is finished
+                return;
+            }
+            AnswerDTO answer = currentQuestion.get().getRightAnswer();
+            // ToDo: get SSEManager to have access to sending methods
+            //myGame.get().getEmitters().sendAll(answer);
         }
     }
 }
