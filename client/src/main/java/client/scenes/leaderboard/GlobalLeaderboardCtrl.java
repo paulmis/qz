cd2995fd4ae7329681c7f0b74d2a81256fcc6fd9
David Dinucu-Jianu <D.dinucu-jianu@student.tudelft.nl>
2022-03-08 15:35:56 +0100

    Made some improvements in the design

--- client/src/main/java/client/scenes/leaderboard/GlobalLeaderboardCtrl.java
+++ client/src/main/java/client/scenes/leaderboard/GlobalLeaderboardCtrl.java
@@ -1,4 +1,44 @@
 package client.scenes.leaderboard;
 
-public class GlobalLeaderboardCtrl {
+import com.jfoenix.controls.JFXListView;
+import commons.entities.UserDTO;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GlobalLeaderboardCtrl implements Initializable {
+    @FXML
+    private ListView usersList;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+       // test.itemsProperty().addListener((observable, oldValue, newValue) -> {
+        //    test.setPrefHeight(test.getItems().size()*50);
+        //});
+
+      //  usersList.setPrefHeight(10000*50);
+      ///  usersList.setMinHeight(10000*50);
+      //  usersList.setMaxHeight(10000*50);
+
+       // javafx.application.Platform.runLater(()->
+      //  {
+      //      for (int i = 0; i < 10000; i++) {
+      //          var user = new UserDTO();
+      //          user.setUsername("david");
+       //         user.setGamesPlayed(100);
+       //         user.setScore(123);
+        //        usersList.getItems().add(new LeaderboardEntryPane(user, i + 1));
+       //         //usersList.getChildren().add(new LeaderboardEntryPane(user,i+1));
+        //    }
+        //});
+
+
+
+
+    }
 }
