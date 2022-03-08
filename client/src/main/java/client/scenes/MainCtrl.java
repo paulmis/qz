a140ee5470a79e528806aafe3a9de2d06abb4976
David Dinucu-Jianu <D.dinucu-jianu@student.tudelft.nl>
2022-03-08 17:12:17 +0100

    Added resizing strategies

--- client/src/main/java/client/scenes/MainCtrl.java
+++ client/src/main/java/client/scenes/MainCtrl.java
@@ -25,10 +25,18 @@ import client.scenes.lobby.configuration.ConfigurationScreenCtrl;
 import client.scenes.lobby.configuration.ConfigurationScreenPane;
 import commons.entities.game.configuration.GameConfigurationDTO;
 import commons.entities.game.configuration.SurvivalGameConfigurationDTO;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.transform.Scale;
 import javafx.stage.Modality;
 import javafx.stage.Popup;
 import javafx.stage.Stage;
@@ -45,22 +53,22 @@ public class MainCtrl {
     private Stage primaryStage;
 
     private ServerConnectScreenCtrl serverConnectScreenCtrl;
-    private Scene serverConnectScreen;
+    private Parent serverConnectScreen;
     
     private LogInScreenCtrl logInScreenCtrl;
-    private Scene logInScreen;
+    private Parent logInScreen;
 
     private RegisterScreenCtrl registerScreenCtrl;
-    private Scene registerScreen;
+    private Parent registerScreen;
 
     private NicknameScreenCtrl nicknameScreenCtrl;
-    private Scene nicknameScreen;
+    private Parent nicknameScreen;
 
     private LobbyScreenCtrl lobbyScreenCtrl;
-    private Scene lobbyScene;
+    private Parent lobbyScene;
 
     private GameScreenCtrl gameScreenCtrl;
-    private Scene gameScreen;
+    private Parent gameScreen;
 
     private Popup lobbySettingsPopUp;
 
@@ -78,42 +86,57 @@ public class MainCtrl {
                            Pair<GameScreenCtrl, Parent> gameScreen) {
         this.primaryStage = primaryStage;
 
-        this.serverConnectScreen = new Scene(serverConnectScreen.getValue());
+        this.serverConnectScreen = serverConnectScreen.getValue();
         this.serverConnectScreenCtrl = serverConnectScreen.getKey();
 
-        this.logInScreen = new Scene(logInScreen.getValue());
+        this.logInScreen = logInScreen.getValue();
         this.logInScreenCtrl = logInScreen.getKey();
 
-        this.registerScreen = new Scene(registerScreen.getValue());
+        this.registerScreen = registerScreen.getValue();
         this.registerScreenCtrl = registerScreen.getKey();
 
-        this.nicknameScreen = new Scene(nicknameScreen.getValue());
+        this.nicknameScreen = nicknameScreen.getValue();
         this.nicknameScreenCtrl = nicknameScreen.getKey();
-        
-        this.lobbyScreenCtrl = lobbyScreen.getKey();
-        this.lobbyScene = new Scene(lobbyScreen.getValue());
 
-        this.gameScreen = new Scene(gameScreen.getValue());
+        this.lobbyScene = lobbyScreen.getValue();
+        this.lobbyScreenCtrl = lobbyScreen.getKey();
+
+        this.gameScreen = gameScreen.getValue();
         this.gameScreenCtrl = gameScreen.getKey();
 
         primaryStage.getIcons().add(new Image(getClass().getResource("/client/images/logo.png").toExternalForm()));
 
         lobbySettingsPopUp = new Popup();
         showServerConnectScreen();
-        primaryStage.show();
     }
 
+    public enum StageScalingStrategy {
+        Identity,
+        Stretch,
+        Letterbox,
+    }
+    private void showScreenLetterBox(Parent parent, StageScalingStrategy strategy) {
+        if(primaryStage.getScene()==null)
+            primaryStage.setScene(new Scene(new Group(new StackPane(parent))));
+
+        primaryStage.setTitle("Quizzzzz");
+        var scene = primaryStage.getScene();
+        var group = ((Group)scene.getRoot());
+        var pane = (StackPane)group.getChildren().get(0);
+        pane.getChildren().set(0,parent);
+        StackPane.setAlignment(pane, Pos.CENTER_LEFT);
+        primaryStage.setMinHeight(576);
+        primaryStage.setMinWidth(1024);
+        primaryStage.show();
+        scaling(scene, pane, 1024,576, strategy);
+    }
 
     /**
      * This function displays the register screen.
      * It also sets it min width and height
      */
     public void showServerConnectScreen() {
-        primaryStage.setTitle("Server Connect Screen");
-        primaryStage.setScene(serverConnectScreen);
-        primaryStage.sizeToScene();
-        primaryStage.setMinHeight(500);
-        primaryStage.setMinWidth(500);
+        this.showScreenLetterBox(serverConnectScreen, StageScalingStrategy.Letterbox);
     }
 
     /**
@@ -121,11 +144,7 @@ public class MainCtrl {
      * It also sets it min width and height.
      */
     public void showLogInScreen() {
-        primaryStage.setTitle("Log In Screen");
-        primaryStage.setScene(logInScreen);
-        primaryStage.sizeToScene();
-        primaryStage.setMinHeight(500);
-        primaryStage.setMinWidth(500);
+        this.showScreenLetterBox(logInScreen, StageScalingStrategy.Letterbox);
     }
 
     /**
@@ -133,11 +152,7 @@ public class MainCtrl {
      * It also sets it min width and height
      */
     public void showRegisterScreen() {
-        primaryStage.setTitle("Register Screen");
-        primaryStage.setScene(registerScreen);
-        primaryStage.sizeToScene();
-        primaryStage.setMinHeight(500);
-        primaryStage.setMinWidth(500);
+        this.showScreenLetterBox(registerScreen, StageScalingStrategy.Letterbox);
     }
 
     /**
@@ -145,23 +160,14 @@ public class MainCtrl {
      * It also sets it min width and height
      */
     public void showNicknameScreen() {
-        nicknameScreenCtrl.reset();
-        primaryStage.setTitle("Nickname Screen");
-        primaryStage.setScene(nicknameScreen);
-        primaryStage.sizeToScene();
-        primaryStage.setMinHeight(500);
-        primaryStage.setMinWidth(500);
+        this.showScreenLetterBox(nicknameScreen, StageScalingStrategy.Letterbox);
     }
 
     /**
      * Shows the lobby screen.
      */
     public void showLobbyScreen() {
-        primaryStage.setTitle(lobbyScreenCtrl.getName());
-        primaryStage.setScene(lobbyScene);
-        primaryStage.sizeToScene();
-        primaryStage.setMinHeight(500);
-        primaryStage.setMinWidth(500);
+        this.showScreenLetterBox(lobbyScene, StageScalingStrategy.Letterbox);
     }
 
     /**
@@ -169,11 +175,7 @@ public class MainCtrl {
      * It also sets it min width and height.
      */
     public void showGameScreen() {
-        primaryStage.setTitle("Game Screen");
-        primaryStage.setScene(gameScreen);
-        primaryStage.sizeToScene();
-        primaryStage.setMinHeight(500);
-        primaryStage.setMinWidth(500);
+        this.showScreenLetterBox(gameScreen, StageScalingStrategy.Identity);
     }
 
     /**
@@ -221,5 +223,76 @@ public class MainCtrl {
      */
     public void closeLobbySettings() {
         lobbySettingsPopUp.hide();
+    }
+
+    SceneSizeChangeListener sizeListener =
+            null;
+
+    private void scaling(final Scene scene,
+                         final Pane contentPane, float initWidth, float initHeight, StageScalingStrategy strategy) {
+        if(sizeListener!=null) {
+            scene.widthProperty().removeListener(sizeListener);
+            scene.heightProperty().removeListener(sizeListener);
+        }
+
+        final double ratio = initWidth / initHeight;
+
+        sizeListener = new SceneSizeChangeListener(scene, ratio, initHeight, initWidth, contentPane, strategy);
+
+        scene.widthProperty().addListener(sizeListener);
+        scene.heightProperty().addListener(sizeListener);
+    }
+
+    private static class SceneSizeChangeListener implements ChangeListener<Number> {
+        private final Scene scene;
+        private final double ratio;
+        private final double initHeight;
+        private final double initWidth;
+        private final Pane contentPane;
+        private final StageScalingStrategy strategy;
+
+        public SceneSizeChangeListener(Scene scene, double ratio, double initHeight, double initWidth,
+                                       Pane contentPane, StageScalingStrategy strategy) {
+            this.scene = scene;
+            this.ratio = ratio;
+            this.initHeight = initHeight;
+            this.initWidth = initWidth;
+            this.contentPane = contentPane;
+            this.strategy = strategy;
+        }
+
+        @Override
+        public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) {
+            System.out.println(initHeight);
+            final double newWidth = scene.getWidth();
+            final double newHeight = scene.getHeight();
+
+            switch(strategy) {
+                case Identity:
+                    contentPane.setPrefWidth(newWidth);
+                    contentPane.setPrefHeight(newHeight);
+                    break;
+                case Stretch:
+                    Scale scaling = new Scale(newWidth / initWidth, newHeight / initHeight);
+                    scaling.setPivotX(0);
+                    scaling.setPivotY(0);
+                    scene.getRoot().getTransforms().setAll(scaling);
+                    contentPane.setPrefWidth(newWidth / (newWidth / initWidth));
+                    contentPane.setPrefHeight(newHeight / (newHeight / initHeight));
+                    break;
+                case Letterbox:
+                    double scaleFactor =
+                            newWidth / newHeight > ratio
+                                    ? newHeight / initHeight
+                                    : newWidth / initWidth;
+                    Scale letterScale = new Scale(scaleFactor, scaleFactor);
+                    letterScale.setPivotX(0);
+                    letterScale.setPivotY(0);
+                    scene.getRoot().getTransforms().setAll(letterScale);
+                    contentPane.setPrefWidth(newWidth / scaleFactor);
+                    contentPane.setPrefHeight(newHeight / scaleFactor);
+                    break;
+            }
+        }
     }
 }
