c66cb41a6ac559a83018c9bb14b295288b26d51b
rstular <r.stular@student.tudelft.nl>
2022-03-13 00:23:51 +0100

    feat(db-pop): Add QuestionController and ActivityController

---
+++ server/src/main/java/server/api/ActivityController.java
@@ -0,0 +1,39 @@
+package server.api;
+
+import commons.entities.ActivityDTO;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import server.database.entities.question.Activity;
+import server.database.repositories.question.ActivityRepository;
+
+/**
+ * Controller for adding activities.
+ */
+@RestController
+@RequestMapping("/api/activity")
+public class ActivityController {
+    @Autowired
+    private ActivityRepository activityRepository;
+
+    /**
+     * Batch create/update activities.
+     *
+     * @param activities List of activities to add.
+     */
+    @PostMapping("/add")
+    ResponseEntity<HttpStatus> addActivity(@RequestBody List<ActivityDTO> activities) {
+        // Convert DTOs to entities
+        List<Activity> activityList = activities.stream().map(Activity::new).collect(Collectors.toList());
+        // Save the entities
+        activityRepository.saveAll(activityList);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
---
+++ server/src/main/java/server/api/QuestionController.java
@@ -0,0 +1,48 @@
+package server.api;
+
+import java.util.List;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import server.database.entities.question.Activity;
+import server.database.entities.question.MCQuestion;
+import server.database.entities.question.Question;
+import server.database.repositories.question.QuestionRepository;
+import server.services.ActivityService;
+
+/**
+ * Question management controller.
+ */
+@RestController
+@RequestMapping("/api/question")
+public class QuestionController {
+    @Autowired
+    private ActivityService activityService;
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    /**
+     * Create a new random question.
+     *
+     * @return Whether the question was successfully created.
+     */
+    @PutMapping("/mc/create")
+    public ResponseEntity<HttpStatus> mcCreate() {
+        List<Activity> activities = activityService.getActivities(4);
+
+        Question question = new MCQuestion(UUID.randomUUID(),
+                activities,
+                "Question X",
+                activities.get(0),
+                true);
+
+        questionRepository.save(question);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
