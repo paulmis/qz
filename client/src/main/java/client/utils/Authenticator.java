32c404dfb95ffd5aad63f9eaade15d02bdc9bd7c
Cojocaru Alexandru-Gabriel <A.G.Cojocaru-2@student.tudelft.nl>
2022-03-13 12:17:31 +0100

    Fixed checkstyle

--- client/src/main/java/client/utils/Authenticator.java
+++ client/src/main/java/client/utils/Authenticator.java
@@ -5,7 +5,7 @@ import jakarta.ws.rs.client.ClientRequestFilter;
 import jakarta.ws.rs.core.MultivaluedMap;
 
 /**
- * Class that handles authentication additional information
+ * Class that handles authentication additional information.
  */
 public class Authenticator implements ClientRequestFilter {
 
@@ -17,8 +17,9 @@ public class Authenticator implements ClientRequestFilter {
 
     /**
      * Function that acts as a middleware in order to
-     * keep track of the token on every request we make
-     * @param requestContext
+     * keep track of the token on every request we make.
+     *
+     * @param requestContext context holder for request.
      */
     public void filter(ClientRequestContext requestContext) {
         MultivaluedMap<String, Object> headers = requestContext.getHeaders();
