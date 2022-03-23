bb52504b7c6f0766231f115f56170ffb93849373
p.m.misterka@student.tudelft.nl <p.m.misterka@student.tudelft.nl>
2022-03-24 00:17:17 +0100

    add putAnswer, AnswerHandler, rework MCPane

--- client/src/main/java/client/scenes/questions/MCQuestionPane.java
+++ client/src/main/java/client/scenes/questions/MCQuestionPane.java
@@ -1,7 +1,7 @@
 package client.scenes.questions;
 
-import java.net.URL;
-import java.util.List;
+import client.communication.game.AnswerHandler;
+import commons.entities.questions.MCQuestionDTO;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -15,25 +15,18 @@ import lombok.Generated;
  * generation of the control inside code.
  */
 @Generated
-public class MultipleChoiceQuestionPane extends StackPane {
+public class MCQuestionPane extends StackPane {
 
     private Node view;
-    private MultipleChoiceQuestionCtrl controller;
-
+    private MCQuestionCtrl controller;
 
     /** Creates the Node view of the control
      * and adds it to the children of the StackPane
      * with the given parameters.
      *
-     * @param questionText The question in string format.
-     * @param answersImages A list of url images of the answers.
-     * @param answersText A list of texts of the answers.
-     * @param actions A list of actions that each option should perform.
+     * @param question the question to be displayed
      */
-    public MultipleChoiceQuestionPane(String questionText,
-                                      List<String> answersText,
-                                      List<URL> answersImages,
-                                      List<MultipleChoiceQuestionCtrl.AnswerHandler> actions) {
+    public MCQuestionPane(MCQuestionDTO question, AnswerHandler answerHandler) {
 
         // We create the loader for the fxml of the question
         FXMLLoader fxmlLoader =
@@ -42,7 +35,7 @@ public class MultipleChoiceQuestionPane extends StackPane {
         // We set the controller of the fxml to our newly created controller
         // we also pass in the question text and the answer handler
         fxmlLoader.setControllerFactory(param ->
-                controller = new MultipleChoiceQuestionCtrl(questionText, answersImages, answersText, actions));
+                controller = new MCQuestionCtrl(question, answerHandler));
 
         // This loads the fxml
         try {
