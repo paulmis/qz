a0f51bd677af81b931006f51e9bcb93f77f1a816
rstular <r.stular@student.tudelft.nl>
2022-03-25 02:38:17 +0100

    fix(timing): Optimize the FSM management

--- server/src/main/java/server/services/fsm/GameFSM.java
+++ server/src/main/java/server/services/fsm/GameFSM.java
@@ -8,16 +8,36 @@ import server.database.entities.game.Game;
  * The GameFSM class is a Finite State Machine that is used to manage a game.
  */
 @Data
-public abstract class GameFSM implements Runnable {
+public abstract class GameFSM {
+    /**
+     * Game that is being managed.
+     */
     @NonNull private final Game game;
 
+    /**
+     * Execution context of the FSM.
+     */
     @NonNull private final FSMContext context;
 
+    /**
+     * Indicates whether the FSM is currently running.
+     */
+    private boolean running = false;
+
+    /**
+     * The next state to transition to.
+     */
     private FSMFuture future;
 
     /**
      * Run the finite state machine.
      */
-    @Override
     public abstract void run();
+
+    /**
+     * Stop the FSM.
+     */
+    public void stop() {
+        running = false;
+    }
 }
