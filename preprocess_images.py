422fd7c058c57539e7308af398a97a4cc24cc29a
David Dinucu-Jianu <D.dinucu-jianu@student.tudelft.nl>
2022-03-27 19:39:50 +0200

    Processed all images to be square

--- preprocess_images.py
+++ preprocess_images.py
@@ -7,19 +7,16 @@ import seam_carving
 path = sys.argv[1]
 
 
-for filename in glob.iglob(path + '**/*.jpeg', recursive=True):
+for filename in glob.iglob(path + '**/*.jpg', recursive=True):
     print(filename)
     rgb_im = Image.open(filename).convert('RGB')
-    rgb_im.thumbnail((500,500,3), Image.ANTIALIAS)
-    rgb_im = np.asarray(rgb_im, dtype=np.float32) / 255
-    src = rgb_im[:, :, :3]
+    rgb_im.thumbnail((500,500), Image.ANTIALIAS)
+    src = np.asarray(rgb_im, dtype=np.float32)
     print(src.shape)
     if len(src.shape) == 2:
         src_h, src_w = src.shape
     else:
         src_h, src_w, _ = src.shape
-    dx = min(src_h, src_w) - src_w
-    dy = min(src_h, src_w) - src_h
     dst = seam_carving.resize(
         src, (min(src_h, src_w), min(src_h, src_w)),
         energy_mode='backward',   # Choose from {backward, forward}
