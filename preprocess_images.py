1e574346e84da0868f76b722063106019093438b
David Dinucu-Jianu <D.dinucu-jianu@student.tudelft.nl>
2022-03-27 19:18:44 +0200

    modified

--- preprocess_images.py
+++ preprocess_images.py
@@ -3,13 +3,16 @@ import glob
 import numpy as np
 from PIL import Image
 import subprocess
-
+import seam_carving
 path = sys.argv[1]
 
 
 for filename in glob.iglob(path + '**/*.jpeg', recursive=True):
     print(filename)
-    src = np.array(Image.open(filename))
+    rgb_im = Image.open(filename).convert('RGB')
+    rgb_im.thumbnail((500,500,3), Image.ANTIALIAS)
+    rgb_im = np.asarray(rgb_im, dtype=np.float32) / 255
+    src = rgb_im[:, :, :3]
     print(src.shape)
     if len(src.shape) == 2:
         src_h, src_w = src.shape
@@ -17,5 +20,10 @@ for filename in glob.iglob(path + '**/*.jpeg', recursive=True):
         src_h, src_w, _ = src.shape
     dx = min(src_h, src_w) - src_w
     dy = min(src_h, src_w) - src_h
-    print((dx,dy))
-    #subprocess.Popen("python seam_carving.py -resize -dx {} -dy {} -im {} -out {}".format(dx,dy,filename,filename), shell = True, stdout=subprocess.PIPE)
+    dst = seam_carving.resize(
+        src, (min(src_h, src_w), min(src_h, src_w)),
+        energy_mode='backward',   # Choose from {backward, forward}
+        order='width-first',  # Choose from {width-first, height-first}
+        keep_mask=None
+    )
+    Image.fromarray(dst).save(filename)
